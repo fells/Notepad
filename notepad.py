d8a576c16d7bc4b7170aa28232cf04a26bd3a5de
Mihel Dubois Calil <micheldcalil@hotmail.com>
2020-06-11 02:17:39 -0300

    Made the notepad  Skeleton

---
+++ notepad.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+if __name__ == "__main__":
+    #Basic tkinter setup
+    root = Tk()
+    root.title("Untitled - Notepad")
+    # root.wm_iconbitmap("1.ico")
+    root.geometry("644x788")
+
+    root.mainloop()
