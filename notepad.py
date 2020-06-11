6d8758359dfd667ccb0346d890d41e5a7edab5f1
Mihel Dubois Calil <micheldcalil@hotmail.com>
2020-06-11 02:25:21 -0300

    Added the letter and a menu bar

--- notepad.py
+++ notepad.py
@@ -2,10 +2,20 @@ from tkinter import *
 
 
 if __name__ == "__main__":
-    #Basic tkinter setup
+    # Basic tkinter setup
     root = Tk()
     root.title("Untitled - Notepad")
-    # root.wm_iconbitmap("1.ico")
     root.geometry("644x788")
+
+    # Add text area
+
+    TextArea = Text (root, font="Times 13")
+    file = None
+
+    # Lets create a menu bar
+    MenuBar = Menu(root)
+    FileMenu = Menu(MenuBar, tearoff = 0)
+
+    # To open new file
 
     root.mainloop()
